12a8b6f937b8a4a169e0bdd68d6e832da8a59242
Facephi <developer@facephi.com>
2026-08-05 08:31:41 +0200

    New SPM Version: 1.0.3

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "git@github.com:facephi/FPHIBehaviorSDK-SPM.git", .exactItem("6.33.1")),
-        .package(url: "git@github.com:facephi/FPHILicenseActivator-SPM.git", .exactItem("1.0.1")),
+        .package(url: "git@github.com:facephi/FPHILicenseActivator-SPM.git", .exactItem("1.0.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -30,8 +30,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FPHIBehavior",
-            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHIBehavior/1.0.1/FPHIBehavior.zip",
-            checksum: "abb52d280b44963f37ba5ab108d5b1801c5f57eb8452dead4cfae9176500d86e"
+            url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/FPHIBehavior/1.0.3/FPHIBehavior.zip",
+            checksum: "0da2178d30c19f8576221f4bd9a1625ad6102cf1ee6c15c0babcc742ea096564"
         ),
     ]
 )
